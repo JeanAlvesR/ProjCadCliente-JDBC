b8519c8e17e818ce8f02f8c2ec9c18786df417c3
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-03-02 14:09:31 -0300

    Update Cliente.java

--- src/model/entities/Cliente.java
+++ src/model/entities/Cliente.java
@@ -5,23 +5,23 @@ import java.util.Objects;
 
 public class Cliente implements Serializable {
 
-    private Integer Id;
+    private Integer id;
     private String name;
 
     public Cliente(){}
 
 
     public Cliente(Integer id, String name) {
-        Id = id;
+        this.id = id;
         this.name = name;
     }
 
     public Integer getId() {
-        return Id;
+        return id;
     }
 
     public void setId(Integer id) {
-        Id = id;
+        this.id = id;
     }
 
     public String getName() {
@@ -32,24 +32,24 @@ public class Cliente implements Serializable {
         this.name = name;
     }
 
-    //Utilizando para comparar com o nome
+    //Comparando por Id
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Cliente cliente = (Cliente) o;
-        return name.equals(cliente.name);
+        return id.equals(cliente.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(id);
     }
 
     @Override
     public String toString() {
         return "Cliente{" +
-                "Id=" + Id +
+                "Id=" + id +
                 ", name='" + name + '\'' +
                 '}';
     }
